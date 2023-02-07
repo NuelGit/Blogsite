034dadc4505b56d9782376bb7885edb34b7c794d
NuelGit <zioneemma@gmail.com>
2023-02-07 21:25:58 +0100

    updated express

--- src/Extra.js
+++ src/Extra.js
@@ -1,34 +1,6 @@
 import express from 'express'
 
-   let articlesInfor = [{
-
-    name: 'Learn-React',
-    upvotes: 0,
-   },
-
-{
-    name: 'Lear-Node',
-    upvotes: 0,
-},
-
-{
-    name: 'Lear-MongoDb',
-    upvotes: 0,
-}]
-
-    const app = express()
-    app.use(express.json())
-app.get('api/articles/:name/upvote', (req, res) =>{
-    const {name} = req.params
-    const article = articlesInfor.find(a => a.name ===name)
-    if(article){
-        article.upvotes += 1
-        res.send(`The ${name} article now has ${article.upvotes} upvotes`)
-    }else {
-        res.send("doesn't exist ")
-    }
-    
+   
     app.listen(8000, ()=>{
         console.log('Server listening on port 8000')
     })
-})
--- src/server.js
+++ src/server.js
@@ -3,7 +3,32 @@ import express from 'express'
 const app = express()
 app.use(express.json())
 
+let articlesInfor = [{
 
+    name: 'learn-react',
+    upvotes: 0,
+   },
+
+{
+    name: 'learn-node',
+    upvotes: 0,
+},
+
+{
+    name: 'mongoDb',
+    upvotes: 0,
+}]
+
+app.put('/api/articles/:name/upvote', (req, res) =>{
+    const {name} = req.params
+    const article = articlesInfor.find(a => a.name ===name)
+    if(article){
+        article.upvotes += 1
+        res.send(`The ${name} article now has ${article.upvotes} upvotes`)
+    }else {
+        res.send("doesn't exist ")
+    }
+})
 
 app.listen(8000, ()=>{
     console.log('Server is listening on port 8000')
