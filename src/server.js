198ebf5711ad79ab7506261c11217cf971b54087
NuelGit <zioneemma@gmail.com>
2023-02-13 15:56:22 +0100

    updated NodeJS

--- src/server.js
+++ src/server.js
@@ -7,16 +7,19 @@ let articlesInfor = [{
 
     name: 'learn-react',
     upvotes: 0,
+    comments: [],
    },
 
 {
     name: 'learn-node',
     upvotes: 0,
+    comments: [],
 },
 
 {
     name: 'mongoDb',
     upvotes: 0,
+    comments: [],
 }]
 
 app.put('/api/articles/:name/upvote', (req, res) =>{
@@ -24,9 +27,24 @@ app.put('/api/articles/:name/upvote', (req, res) =>{
     const article = articlesInfor.find(a => a.name ===name)
     if(article){
         article.upvotes += 1
-        res.send(`The ${name} article now has ${article.upvotes} upvotes !!!`)
+        res.send(`The ${name} article now has ${article.upvotes} upvotes!! `)
     }else {
         res.send("doesn't exist ")
+    }
+})
+
+app.post('/api/articles/:name/comments', (req, res) =>{
+    const{name} = req.params
+    const {postedBy, text}= req.body
+
+    const article = articlesInfor.find(a => a.name ===name)
+
+    if(article){
+
+        article.comments.push({postedBy, text})
+        res.send(article.comments)
+    } else{
+        res.send("doesn't exist get out")
     }
 })
 
